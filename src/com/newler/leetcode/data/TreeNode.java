3d317fa65fa6491ff6038aa7f22078c496c36d85
刘乐 <liule_17173@cyou-inc.com>
2020-06-23 18:46:54 +0800

    完成二叉树前序遍历

---
+++ src/com/newler/leetcode/data/TreeNode.java
@@ -0,0 +1,11 @@
+package com.newler.leetcode.data;
+
+/**
+ * Created by liule on 2020/6/23 9:36
+ **/
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    TreeNode(int x) { val = x; }
+}
