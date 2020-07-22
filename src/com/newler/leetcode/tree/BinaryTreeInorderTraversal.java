a82f05195d8b4067a731469f9061bcf0b09cf2bb
LeLe673893702 <673893702@qq.com>
2020-07-23 00:00:43 +0800

    增加二叉树前序、中序、后序的非递归遍历

--- src/com/newler/leetcode/tree/BinaryTreeInorderTraversal.java
+++ src/com/newler/leetcode/tree/BinaryTreeInorderTraversal.java
@@ -1,28 +1,31 @@
 package com.newler.leetcode.tree;
-// [145]、二叉树的后序遍历
-// 2020年7月15日21:54:07
-//给定一个二叉树，返回它的 后序 遍历。 
+// [94]、二叉树的中序遍历
+
+//给定一个二叉树，返回它的中序 遍历。 
 //
 // 示例: 
 //
-// 输入: [1,null,2,3]  
+// 输入: [1,null,2,3]
 //   1
 //    \
 //     2
 //    /
-//   3 
+//   3
 //
-//输出: [3,2,1] 
+//输出: [1,3,2] 
 //
 // 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
-// Related Topics 栈 树
+// Related Topics 栈 树 哈希表
 
 
 import com.newler.leetcode.data.TreeNode;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
 
-public class BinaryTreePostorderTraversal {
+public class BinaryTreeInorderTraversal {
     //leetcode submit region begin(Prohibit modification and deletion)
 
     /**
@@ -34,38 +37,39 @@ public class BinaryTreePostorderTraversal {
      * TreeNode(int x) { val = x; }
      * }
      */
-    // 递归
     class Solution {
         private List<Integer> results = new ArrayList<>();
 
-        public List<Integer> postorderTraversal(TreeNode root) {
+        public List<Integer> inorderTraversal(TreeNode root) {
             travesal(root);
             return results;
         }
 
         public void travesal(TreeNode root) {
             if (root == null) return;
-            postorderTraversal(root.left);
-            postorderTraversal(root.right);
+            travesal(root.left);
+            travesal(root.right);
             results.add(root.val);
         }
+
     }
 
-    // 非递归，模拟栈
-    class Solutionn {
-        public List<Integer> postorderTraversal(TreeNode root) {
-            if (root == null) return  Collections.emptyList();
+    // 非递归
+    class Solution2 {
+
+        public List<Integer> inorderTraversal(TreeNode root) {
+            if (root == null) return Collections.emptyList();
 
             Stack<TreeNode> stack = new Stack<TreeNode>();
             List<Integer> results = new ArrayList<>();
             stack.push(root);
 
-            while(!stack.isEmpty()) {
+            while (!stack.isEmpty()) {
                 TreeNode top = stack.pop();
                 if (top != null) {
+                    if (top.right != null) stack.push(top.right);
                     stack.push(top);
                     stack.push(null);
-                    if (top.right != null) stack.push(top.right);
                     if (top.left != null) stack.push(top.left);
                 } else {
                     results.add(stack.pop().val);
@@ -73,5 +77,6 @@ public class BinaryTreePostorderTraversal {
             }
             return results;
         }
+
     }
 }
